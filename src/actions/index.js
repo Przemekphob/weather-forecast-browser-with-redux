2ce599230ccf7fee92d99f1a3ccae688cccb4b46
Przemek Mazur <Przemek1713@gmail.com>
2018-05-07 22:17:34 +0200

    weather list container added, chart component added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '747811aa0a914006c0b9a397eac8787f';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
