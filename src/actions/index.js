61351eb2ab1752b0319ef327d7873378a1406326
Przemek Mazur <Przemek1713@gmail.com>
2018-05-07 19:01:43 +0200

    api key and request added

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '747811aa0a914006c0b9a397eac8787f';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: 'FETCH_WEATHER',
+		payload: request
+	};
+}
